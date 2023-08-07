8ba94bae6f5f9cb6ce05138bcf8095ed716dcd3e
One_AR <world127617@outlook.com>
2023-08-07 14:55:18 +0800

    主界面10%

--- app/src/main/java/com/midream/schatapp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/midream/schatapp/ui/gallery/GalleryFragment.java
@@ -10,21 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.midream.schatapp.databinding.FragmentGalleryBinding;
+import com.midream.schatapp.databinding.FragmentHomeBinding;
 
 public class GalleryFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+    private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
+        final TextView textView = binding.textHome;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
